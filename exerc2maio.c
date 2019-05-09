66e0f68522c18916f33d69f397b9f8fd635f8435
analuciaa <49961695+analuciaa@users.noreply.github.com>
2019-05-09 11:01:34 -0300

    Create exerc2maio.c

---
+++ exerc2maio.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+void main(){
+char cadastro [2][100];
+char a;
+printf("Nome do pai:\n");
+scanf(%{^\n}%"c",cadastro[1]);
+printf("Nome da mãe:\n");
+scanf(%{^\n}%"c",cadastro[2]);
+ for(i = 1; i < 2; i++){
+ printf("%s\n",cadastro[i]);
+     }
+  }  for(i=0;i<100;i++);
+  if([i]==[a]{
+  a++;
+  printf("A quantidade de A é:\n",a);
+  
+}
